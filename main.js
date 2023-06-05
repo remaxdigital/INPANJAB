cc2a615ab4e09d6417752e087c5c24f6626c408a
remaxdigital <109919240+remaxdigital@users.noreply.github.com>
2023-06-05 22:18:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -27,10 +27,7 @@ var fonts = [
 window.addEventListener('scroll', function() {
     var nameElement = document.querySelector('#centered-name');
     var scrollPosition = window.pageYOffset;
-
-    // Change font size based on scroll position
-    nameElement.style.fontSize = 20 + scrollPosition * 0.01 + 'px';
-
+    
     // Change font family based on scroll position
     var fontIndex = Math.floor(scrollPosition / 100) % fonts.length;
     nameElement.style.fontFamily = fonts[fontIndex];
@@ -43,29 +40,11 @@ window.addEventListener('scroll', function() {
 
     const link = document.querySelectorAll('nav > .hover-this');
     const cursor = document.querySelector('.cursor');
-
-    const animateit = function (e) {
-          const heading = this.querySelector('h1');
-          const { offsetX: x, offsetY: y } = e,
-          { offsetWidth: width, offsetHeight: height } = this,
-
-          move = 15,
-          xMove = x / width * (move * 2) - move,
-          yMove = y / height * (move * 2) - move;
-
-          heading.style.transform = `translate(${xMove}px, ${yMove}px)`;
-
-          if (e.type === 'mouseleave') heading.style.transform = '';
-    };
-
     const editCursor = e => {
           const { clientX: x, clientY: y } = e;
           cursor.style.left = x + 'px';
           cursor.style.top = y + 'px';
     };
-
-    link.forEach(b => b.addEventListener('mousemove', animateit));
-    link.forEach(b => b.addEventListener('mouseleave', animateit));
     window.addEventListener('mousemove', editCursor);
 
 })();
